cce62d253f6ac3a8e1aaea3c3fd9493464d41c5a
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-12-12 03:21:47 +0000

    Get multicast audio working

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+mod output;
+
+use crate::output::Output;
+use cpal::traits::{DeviceTrait, HostTrait};
 use rand::random;
 use std::sync::mpsc::channel;
 
-fn write_audio(data: &mut [f32], _: &cpal::OutputCallbackInfo) {
-    for sample in data.iter_mut() {
-        *sample = random::<f32>() / 10.;
-    }
-}
-
 fn main() {
     let host = cpal::default_host();
-    let device = host
-        .default_output_device()
-        .expect("no output device available");
 
+    let mut outputs = Vec::new();
     for device in host.output_devices().unwrap() {
         println!("{}", device.name().unwrap());
         for config in device.supported_output_configs().unwrap() {
@@ -25,38 +20,24 @@ fn main() {
                 config.channels(),
             )
         }
+
+        outputs.push(Output::new(device));
     }
-
-    println!(
-        "{:#?}",
-        host.output_devices()
-            .unwrap()
-            .map(|device| { device.name().unwrap() })
-            .collect::<Vec<_>>()
-    );
-
-    let mut configs = device
-        .supported_output_configs()
-        .expect("error while querying configs");
-    let config = configs
-        .next()
-        .expect("no supported config")
-        .try_with_sample_rate(cpal::SampleRate(48000))
-        .expect("44.1kHz sample rate not supported");
-    let stream = device
-        .build_output_stream(
-            &config.config(),
-            write_audio,
-            move |err| {
-                eprintln!("stream error: {}", err);
-            },
-            None,
-        )
-        .unwrap();
 
     let (sigint_tx, sigint_rx) = channel();
     ctrlc::set_handler(move || sigint_tx.send(()).unwrap()).unwrap();
 
-    stream.play().unwrap();
-    sigint_rx.recv().unwrap();
+    for output in &mut outputs {
+        output.play();
+    }
+
+    loop {
+        if sigint_rx.try_recv().is_ok() {
+            break;
+        };
+        let random_sample = random::<f32>() / 20.;
+        for output in &mut outputs {
+            output.write(&[random_sample])
+        }
+    }
 }
---
+++ src/output.rs
@@ -0,0 +1,55 @@
+use cpal::traits::{DeviceTrait, StreamTrait};
+use cpal::{BufferSize, Device, SampleRate, Stream, StreamConfig};
+use std::collections::VecDeque;
+use std::sync::{Arc, Condvar, Mutex};
+
+pub struct Output {
+    stream: Stream,
+    buffer: Arc<(Mutex<VecDeque<f32>>, Condvar)>,
+}
+
+impl Output {
+    pub fn new(device: Device) -> Self {
+        let name = device.name().unwrap();
+        let buffer = Arc::new((Mutex::new(VecDeque::new()), Condvar::new()));
+        let callback_buffer = buffer.clone();
+
+        Output {
+            stream: device
+                .build_output_stream(
+                    &StreamConfig {
+                        channels: 2,
+                        sample_rate: SampleRate(48000),
+                        buffer_size: BufferSize::Default,
+                    },
+                    move |data: &mut [f32], _| {
+                        let (buffer, condvar) = &*callback_buffer;
+                        println!("Waiting for condvar...");
+                        let mut buffer = condvar
+                            .wait_while(buffer.lock().unwrap(), |buf| buf.len() < data.len())
+                            .unwrap();
+                        println!("Writing sample");
+                        for sample in data {
+                            *sample = buffer.pop_front().unwrap();
+                        }
+                    },
+                    move |error| {
+                        eprintln!("{}: error: {:?}", name, error);
+                    },
+                    None,
+                )
+                .unwrap(),
+            buffer,
+        }
+    }
+
+    pub fn write(&mut self, data: &[f32]) {
+        let (buffer, condvar) = &*self.buffer;
+        buffer.lock().unwrap().extend(data);
+        condvar.notify_one();
+    }
+    
+    pub fn play(&mut self) {
+        self.stream.play().unwrap();
+    }
+}
